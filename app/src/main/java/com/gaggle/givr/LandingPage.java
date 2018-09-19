1af4757f076a9a1b134ce0a1dd3fc62ee6ccf80e
PhlipPhlops <phillkin@gmail.com>
2018-09-18 23:30:19 -0400

    Added project and preliminary GARBAGE for the login page -- 2 B destroyed

---
+++ app/src/main/java/com/gaggle/givr/LandingPage.java
@@ -0,0 +1,24 @@
+package com.gaggle.givr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_landing_page);
+    }
+}
