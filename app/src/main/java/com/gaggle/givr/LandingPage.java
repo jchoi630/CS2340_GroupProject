9bc2f3be18f75284d9d05ff60c8ffa1e2572d07f
PhlipPhlops <phillkin@gmail.com>
2018-09-20 17:45:44 -0400

    Unbroke master build

--- app/src/main/java/com/gaggle/givr/LandingPage.java
+++ app/src/main/java/com/gaggle/givr/LandingPage.java
@@ -11,14 +11,5 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
-
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        //Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        //set content view AFTER ABOVE sequence (to avoid crash)
-        this.setContentView(R.layout.activity_landing_page);
     }
 }
