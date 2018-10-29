ed770dc661840798ee3d5ee4870704f8886e8b3a
Elijah <epluspequalsk@gmail.com>
2018-10-28 21:25:50 -0400

    adding m7 functionality

---
+++ app/src/main/java/com/gaggle/givr/LocationItemPage.java
@@ -0,0 +1,57 @@
+package com.gaggle.givr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LocationItemPage extends AppCompatActivity {
+    Location location;
+
+    TextView name;
+    TextView latitude;
+    TextView longitude;
+    TextView streetAddress;
+    TextView city;
+    TextView state;
+    TextView zip;
+    TextView type;
+    TextView phone;
+    TextView website;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_item_page);
+
+        location = (Location) getIntent().getSerializableExtra("location");
+
+        bindFields();
+        setFields(location);
+    }
+
+    private void bindFields() {
+        name = findViewById(R.id.name);
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+        streetAddress = findViewById(R.id.streetAddress);
+        city = findViewById(R.id.city);
+        state = findViewById(R.id.state);
+        zip = findViewById(R.id.zip);
+        type = findViewById(R.id.type);
+        phone = findViewById(R.id.phone);
+        website = findViewById(R.id.website);
+    }
+
+    private void setFields(Location location) {
+        name.setText(location.getName());
+        latitude.setText(location.getLatitude());
+        longitude.setText(location.getLongitude());
+        streetAddress.setText(location.getStreetAddress());
+        city.setText(location.getCity());
+        state.setText(location.getState());
+        zip.setText(location.getZip());
+        type.setText(location.getType());
+        phone.setText(location.getPhone());
+        website.setText(location.getWebsite());
+    }
+}
