dc74259be13df1ade10fa6a77b8ba94fb2945085
John Choi <you@example.com>
2018-11-16 10:16:47 -0500

    Finished my junits!

--- app/src/test/java/com/gaggle/givr/AddItemPageTest.java
+++ app/src/test/java/com/gaggle/givr/AddItemPageTest.java
@@ -10,6 +10,7 @@ public class AddItemPageTest {
     private Item item2;
     private Item item3;
     private Item item4;
+    private static final int TIMEOUT = 200;
 
     @Before
     public void setUp() {
@@ -22,14 +23,31 @@ public class AddItemPageTest {
 
     }
 
-    @Test
+    @Test (timeout = TIMEOUT)
     public void populateFields() {
-        if (item.getName() != null) {
+        if (item.getLocation() != null && item.getName() != null) {
             assertTrue(item.getName() != null);
         } else {
             assertTrue(item.getName() == null);
         }
 
+        if (item2.getName() != null && item2.getLocation() != null) {
+            assertTrue(item2.getName() != null);
+        } else {
+            assertTrue(item2.getName() == null);
+        }
+
+        if (item3.getName() != null || item3.getLocation() != null) {
+            assertTrue(item3.getName() != null);
+        } else {
+            assertTrue(item3.getName() == null);
+        }
+
+        if (item4.getLocation() == "hello" && item4.getName() != null) {
+            assertTrue(item4.getName() != null);
+        } else {
+            assertTrue(item4.getName() == null);
+        }
 
     }
 }
