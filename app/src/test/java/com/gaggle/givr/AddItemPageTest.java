09a3fb9fb0dcb5c39a2dbdc533b194092517a4cf
John Choi <you@example.com>
2018-11-16 09:00:57 -0500

    finishing my junits asap!

--- app/src/test/java/com/gaggle/givr/AddItemPageTest.java
+++ app/src/test/java/com/gaggle/givr/AddItemPageTest.java
@@ -1,28 +1,35 @@
 package com.gaggle.givr;
 
-
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
-public class AddItemPageTest {
-    @Before
-    public  void setup() {
-        Item item = new Item("temp","mary",23,11,1232);
-        item.getName();
-        item.getQuantity();
+import static org.junit.Assert.*;
 
-        Item item2 = new Item("temp", null , 12, 11, 1123 );
-        item2.getName();
-        item2.getQuantity();
+public class AddItemPageTest {
+    private Item item;
+    private Item item2;
+    private Item item3;
+    private Item item4;
+
+    @Before
+    public void setUp() {
+
+        Item item = new Item ("test", "Emily", 12, 2, 112);
+        Item item2 = new Item ("test", null, 12, 2, 112);
+        Item item3 = new Item (null, "Tony", 12, 2, 3);
+        Item item4 = new Item ("hello","Josh", 12, 0, 2);
+
 
     }
 
     @Test
-    public void populateFields(Item item){
-       assertTrue(  " This is true!", item.getName() == null);
-       assertTrue("This is not true! ", item.getName() == null);
+    public void populateFields() {
+        if (item.getName() != null) {
+            assertTrue(item.getName() != null);
+        } else {
+            assertTrue(item.getName() == null);
+        }
+
 
     }
 }
